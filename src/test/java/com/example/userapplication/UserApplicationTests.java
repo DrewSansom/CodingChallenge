7311ac26c35c7eb0942ae826f183619524917a5e
Drew Sansom <drewsansom@gmail.com>
2021-04-29 14:19:47 -0700

    Get and delete tests created

--- src/test/java/com/example/userapplication/UserApplicationTests.java
+++ src/test/java/com/example/userapplication/UserApplicationTests.java
@@ -1,13 +1,62 @@
 package com.example.userapplication;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserApplicationTests {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void getUserPageShouldReturnAllUsers() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/users"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUserByIDShouldReturnUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/users/{id}", "1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getInvalidUserByIDShouldReturnError() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/users/{id}","44"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deleteValidUserShouldReturnValid() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .delete("/users/{id}", "1"))
+                .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void deleteInvalidUserShouldReturnInvalid() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .delete("/users/{id}", "1"))
+                .andExpect(status().isGone());
     }
 
 }
